d54f04f5d51734a72ba2cd0d4e194cb4ddb4685c
Rychillie <rychillie@hotmail.com>
2021-09-04 15:01:52 -0300

    add Header component

---
+++ src/components/header.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import styles from "../styles/components/Header.module.scss";
+
+const Header = () => {
+  return <header className={styles.header}></header>;
+};
+
+export default Header;
