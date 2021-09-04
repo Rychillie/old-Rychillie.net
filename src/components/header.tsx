e5d92707e9363fc1dceca680a2e1ffdeea72ae3a
Rychillie <rychillie@hotmail.com>
2021-09-04 15:23:17 -0300

    header created

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,35 @@ import Image from "next/image";
 import styles from "../styles/components/Header.module.scss";
 
 const Header = () => {
-  return <header className={styles.header}></header>;
+  return (
+    <header className={styles.header}>
+      <div className={styles.apresentation}>
+        <div className={styles.profileImage}>
+          <Image
+            src={"/rychillie.png"}
+            blurDataURL={"/rychillie.png"}
+            alt="Rychillie"
+            layout="responsive"
+            placeholder="blur"
+            quality={100}
+            width={270}
+            height={270}
+            loading="lazy"
+          />
+        </div>
+        <div className={styles.sideApresentation}>
+          <h1 className={styles.profileApresentation}>
+            Olá, Eu sou <b>Rychillie</b>
+          </h1>
+          <p className={styles.profileDescription}>
+            Desenvolvedor Frontend, estudante de UI/UX Design e criador de
+            conteúdo, compartilhando seu conhecimento com a comunidade.
+          </p>
+          <div className={styles.socialMedia}></div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
