21d41ecc5573a6fc51c75d00543b2626a590f29c
Rychillie <rychillie@hotmail.com>
2021-09-20 00:47:28 -0300

    scale

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -27,7 +27,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta
           name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=5"
         />
 
         <link rel="manifest" href="/manifest.json" />
