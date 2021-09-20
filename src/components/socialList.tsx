cb1eef5025490e2508c995343506defebf2bda5e
Rychillie <rychillie@hotmail.com>
2021-09-20 00:42:41 -0300

    SEO

--- src/components/socialList.tsx
+++ src/components/socialList.tsx
@@ -5,31 +5,51 @@ const SocialList = () => {
   return (
     <ul className={styles.socialMedia}>
       <li>
-        <a href="" target="_blank">
+        <a
+          href="https://twitter.com/rychillie"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src="/icons/twitter.svg" alt="Twitter" />
         </a>
         <span className={styles.socialName}>Twitter</span>
       </li>
       <li>
-        <a href="" target="_blank">
+        <a
+          href="https://instagram.com/rychillie"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src="/icons/instagram.svg" alt="Instagram" />
         </a>
         <span className={styles.socialName}>Instagram</span>
       </li>
       <li>
-        <a href="" target="_blank">
+        <a
+          href="https://github.com/rychillie"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src="/icons/github.svg" alt="Github" />
         </a>
         <span className={styles.socialName}>Github</span>
       </li>
       <li>
-        <a href="" target="_blank">
+        <a
+          href="https://linkedin.com/in/rychillie"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src="/icons/linkedin.svg" alt="Linkedin" />
         </a>
         <span className={styles.socialName}>Linkedin</span>
       </li>
       <li>
-        <a href="" target="_blank">
+        <a
+          href="http://dribbble.com/rychillie"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src="/icons/dribbble.svg" alt="Dribbble" />
         </a>
         <span className={styles.socialName}>Dribbble</span>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "../styles/globals.scss";
+import Head from "next/head";
 import type { AppProps } from "next/app";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
@@ -21,6 +22,41 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+
+        <link rel="manifest" href="/manifest.json" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/icons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/icons/favicon-16x16.png"
+        />
+        <link
+          rel="mask-icon"
+          href="/icons/safari-pinned-tab.svg"
+          color="#0c0c0c"
+        />
+        <meta name="msapplication-TileColor" content="#0c0c0c" />
+        <meta name="theme-color" content="#0c0c0c" />
+      </Head>
       <Component {...pageProps} />
       <Analytics />
     </>
